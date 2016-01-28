e4afed220238faefcf815ead8498b60ffe9136ff
tunderdomb <tunderdomb@gmail.com>
2016-01-28 16:07:34 +0100

    tests

--- test/model/Model.js
+++ test/model/Model.js
@@ -1,27 +1,27 @@
 var assert = require("chai").assert
-var Model = require("../Model")
-var schema = require("../schema/index")
+var Model = require("../../model")
+var schema = require("../../model/schema")
 
-function simple (name, fn) {
+function simple(name, fn) {
   var count = fn.length
   var models = []
   while (count) {
     models.unshift(new Model())
     --count
   }
-  it(name, function () {
+  it(name, function() {
     fn.apply(null, models)
   })
 }
 
-function extended (name, def, fn) {
+function extended(name, def, fn) {
   var SubClass = Model.extend(def)
-  it(name, function () {
+  it(name, function() {
     fn.call(null, new SubClass())
   })
 }
 
-function extendedModel (name, fn) {
+function extendedModel(name, fn) {
   extended(name, {
     schema: {
       string: new schema.StringProperty(),
@@ -33,7 +33,7 @@ function extendedModel (name, fn) {
   }, fn)
 }
 
-function verifyStateUninitialized (model, property) {
+function verifyStateUninitialized(model, property) {
   assert.equal(model.changedPropertyCount, 0)
   assert.isFalse(model.isChanged)
   assert.isFalse(model.isPropertyChanged(property))
@@ -43,7 +43,7 @@ function verifyStateUninitialized (model, property) {
   assert.isNull(model.getChangedValue(property))
   if (model.hasSchema(property)) assert.isNull(model.getDefaultValue(property))
 }
-function verifyStateChanged (model, property, value) {
+function verifyStateChanged(model, property, value) {
   assert.equal(model.changedPropertyCount, 1)
   assert.isTrue(model.isChanged)
   assert.isTrue(model.isPropertyChanged(property))
@@ -52,7 +52,7 @@ function verifyStateChanged (model, property, value) {
   assert.equal(model.getValue(property), value)
   assert.equal(model.getChangedValue(property), value)
 }
-function verifyStateInitialized (model, property, value) {
+function verifyStateInitialized(model, property, value) {
   assert.equal(model.changedPropertyCount, 0)
   assert.isFalse(model.isChanged)
   assert.isFalse(model.isPropertyChanged(property))
@@ -62,16 +62,14 @@ function verifyStateInitialized (model, property, value) {
     assert.equal(model.getValue(property), value)
   }
   // null value
-  else {
-    if (model.hasSchema(property)) {
-      assert.equal(model.getValue(property), model.getDefaultValue(property))
-    }
+  else if (model.hasSchema(property)) {
+    assert.equal(model.getValue(property), model.getDefaultValue(property))
   }
   assert.equal(model.get(property), value)
   assert.isNull(model.getChangedValue(property))
 }
 
-function verifyStates (model, property, value) {
+function verifyStates(model, property, value) {
 
   // 1. Uninitialized
   verifyStateUninitialized(model, property)
@@ -98,17 +96,17 @@ function verifyStates (model, property, value) {
   verifyStateUninitialized(model, property)
 }
 
-function verifySchemaValueChange (model, property, validValue, invalidValues) {
+function verifySchemaValueChange(model, property, validValue, invalidValues) {
   verifyStates(model, property, validValue)
 
-  invalidValues.forEach(function (invalidValue) {
-    assert.throws(function () {
+  invalidValues.forEach(function(invalidValue) {
+    assert.throws(function() {
       model.set(property, invalidValue)
     })
   })
 }
 
-function verifyDefaultValue (model, property) {
+function verifyDefaultValue(model, property) {
   var schema = model.getSchema(property)
   var defaultValue = schema.getDefault()
 
@@ -117,32 +115,32 @@ function verifyDefaultValue (model, property) {
   assert.equal(model.getDefaultValue(property), defaultValue)
 }
 
-describe("Model", function () {
+describe("Model", function() {
 
-  describe("value access", function () {
-    simple("string", function (model) {
+  describe("value access", function() {
+    simple("string", function(model) {
       verifyStates(model, "string", "test")
     })
-    simple("number", function (model) {
+    simple("number", function(model) {
       verifyStates(model, "number", 1)
     })
-    simple("float", function (model) {
+    simple("float", function(model) {
       verifyStates(model, "float", 1.1)
     })
-    simple("boolean", function (model) {
+    simple("boolean", function(model) {
       verifyStates(model, "boolean1", true)
       verifyStates(model, "boolean2", false)
     })
-    simple("array", function (model) {
+    simple("array", function(model) {
       verifyStates(model, "array", [])
     })
-    simple("object", function (model) {
+    simple("object", function(model) {
       verifyStates(model, "object", {})
     })
-    simple("null", function (model) {
+    simple("null", function(model) {
       verifyStates(model, "null", null)
     })
-    simple("undefined", function (model) {
+    simple("undefined", function(model) {
       verifyStates(model, "undefined", undefined)
     })
     // TODO: date tests
@@ -152,19 +150,19 @@ describe("Model", function () {
     //})
   })
 
-  describe("schema", function () {
-    extendedModel("string", function (model) {
+  describe("schema", function() {
+    extendedModel("string", function(model) {
       verifySchemaValueChange(model, "string", "test", [null, undefined, [], {}, 1, false, true, NaN])
     })
-    extendedModel("number", function (model) {
+    extendedModel("number", function(model) {
       verifySchemaValueChange(model, "number", 1, [null, undefined, [], {}, "test", false, true, NaN])
       verifySchemaValueChange(model, "number", 1.1, [null, undefined, [], {}, "test", false, true, NaN])
     })
-    extendedModel("float", function (model) {
+    extendedModel("float", function(model) {
       verifySchemaValueChange(model, "float", 1.1, [null, undefined, [], {}, "test", false, true, NaN])
       verifySchemaValueChange(model, "float", 1, [null, undefined, [], {}, "test", false, true, NaN])
     })
-    extendedModel("boolean", function (model) {
+    extendedModel("boolean", function(model) {
       verifySchemaValueChange(model, "boolean1", true, [null, undefined, [], {}, 1, "test", NaN])
       verifySchemaValueChange(model, "boolean2", false, [null, undefined, [], {}, 1, "test", NaN])
     })
@@ -172,15 +170,15 @@ describe("Model", function () {
   })
 
 
-  describe("default value", function () {
-    extendedModel("string", function (model) {
+  describe("default value", function() {
+    extendedModel("string", function(model) {
       verifyDefaultValue(model, "string", "test")
     })
-    extendedModel("number", function (model) {
+    extendedModel("number", function(model) {
       verifyDefaultValue(model, "number", 1)
       verifyDefaultValue(model, "number", 1.1)
     })
-    extendedModel("boolean", function (model) {
+    extendedModel("boolean", function(model) {
       verifyDefaultValue(model, "boolean1", true)
       verifyDefaultValue(model, "boolean2", false)
     })
