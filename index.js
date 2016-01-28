95e9ecbe7791fa5c7630fb7dbd92ffca5c975b06
tunderdomb <tunderdomb@gmail.com>
2016-01-28 12:24:45 +0100

    electron test

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 var ui = module.exports = {}
 
-ui.data = require("matchbox-dom/data")
-ui.View = require("./view/View")
-ui.Child = require("./view/Child")
-ui.Event = require("./view/EventInit")
-ui.Action = require("./view/ActionInit")
-ui.Modifier = require("./view/ModifierInit")
-ui.SwitchModifier = require("./view/SwitchModifier")
-ui.EnumModifier = require("./view/EnumModifier")
+ui.dom = require("./dom")
+ui.view = require("./view")
